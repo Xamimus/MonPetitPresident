8447ec9a0a3027aa362b77f2508935353d67e2b8
Alex Lri <alexandre.lairie@gmail.com>
2021-11-02 14:19:40 +0100

    [ADD] - Polygon shape

--- components/Box.vue
+++ components/Box.vue
@@ -1,38 +1,73 @@
 <template>
-    <div class="box-container">
+  <div class="box-container">
+    <div class="row">
+      <div class="col"><div class="polygonblue"></div></div>
+      <div class="col">
         <div class="box">
-
+          <div class="row"></div>
+          <div class="row"></div>
         </div>
+      </div>
+      <div class="col"><div class="polygonred"></div></div>
     </div>
+  </div>
 </template>
 
 <style scoped>
-    .box-container{
-        min-height: 95vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;    
-    }
+.box-container {
+  min-height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    .box{
-        background-color: #fff;
-        height: 60vh;
-        width: 45vw;
-        border-radius: 15px;
-        box-shadow: 5px 5px 5px rgb(48, 48, 48);
-    }
+.box {
+  background-color: #fff;
+  height: 60vh;
+  width: 45vw;
+  border-radius: 15px;
+  box-shadow: 5px 5px 5px rgb(48, 48, 48);
+}
+.polygonblue {
+  -webkit-clip-path: polygon(0 0, 0% 100%, 100% 0);
+  clip-path: polygon(0 0, 0% 100%, 100% 0);
+  background-color: #002395;
+  box-shadow: 5px 4px 4px rgba(0, 0, 0, 0.25);
+  width: 110px;
+  height: 97px;
+  position: absolute;
+  box-shadow: 5px 5px 4px rgba(0, 0, 0, 0.25);
+  left: 40px;
+  top: -5px;
+  z-index: 2;
+}
 
-    @media only screen and (max-width: 600px) {
-        .box {
-            height: 50vh;
-            width: 80vw;
-        }
-    }
+.polygonred {
+  -webkit-clip-path: polygon(100% 100%, 0% 100%, 100% 0);
+  clip-path: polygon(100% 100%, 0% 100%, 100% 0);
+  background-color: #ed2939;
+  position: absolute;
+  box-shadow: 5px 5px 4px rgba(0, 0, 0, 0.25);
+  width: 110px;
+  height: 97px;
+  bottom: -5px;
+  right: 40px;
+}
 
-    @media only screen and (min-width: 2000px) {
-        .box {
-            height: 50vh;
-            width: 40vw;
-        }
-    }
+@media only screen and (max-width: 600px) {
+  .box {
+    height: 50vh;
+    width: 80vw;
+  }
+  .polygonblue, .polygonred {
+      display: none;
+  }
+}
+
+@media only screen and (min-width: 2000px) {
+  .box {
+    height: 50vh;
+    width: 40vw;
+  }
+}
 </style>
